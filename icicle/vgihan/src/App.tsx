ee51ecf04a3419a07718283a96de9cd042f0a0b7
vgihan <jum654@naver.com>
2022-07-23 14:11:22 +0900

    feat(IcicleChart): add IcicleChart Component
    - useMemo를 통해 hierarchy 메모이제이션
    - group 생성 후 rect와 text를 group에 추가함
    - svg 내부에서 element의 위치는 group의 transform으로, 크기는 rect의
    width와 height로 설정
    - App.tsx에 컴포넌트 추가

--- icicle/vgihan/src/App.tsx
+++ icicle/vgihan/src/App.tsx
@@ -1,8 +1,13 @@
+import IcicleChart from "components/IcicleChart";
+import data from "data/exam1";
 import React from "react";
-import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <IcicleChart data={data} width={500} height={500} />
+    </div>
+  );
 }
 
 export default App;
