b752cf8b8da1122d56819b693bfd5191926100bb
제제jeje <gkswlgp456@gmail.com>
2022-07-24 22:43:26 +0900

    feat: Upload temperature data in Seoungbuk-ku

--- linechart/jejecrunch/src/App.tsx
+++ linechart/jejecrunch/src/App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import "./App.css";
+import LineChart from "./components/Linechart";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <LineChart />
             </header>
         </div>
     );
