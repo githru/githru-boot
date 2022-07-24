d15e8afe7557be6cfff53d28acf5e90f7df6268f
momomingzhi <momomingzhi@gmail.com>
2022-07-24 23:07:09 +0900

    feat: line chart 추가

--- linechart/momomingzhi/linechart-tutorial/src/App.tsx
+++ linechart/momomingzhi/linechart-tutorial/src/App.tsx
@@ -1,20 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as d3 from 'd3';
+import { useRef, useEffect } from 'react';
 
 function App() {
+    const svgRef = useRef(null);
+
+    useEffect(() => {
+        const setData = async () => {
+            let parseTime = d3.timeParse('%Y.%m.%d') ?? null;
+            let data = await d3.csv('./COVID19.csv');
+            data.forEach(value => {
+                value.date = parseTime(value?.date?.replace(/\s/g, '') as unknown as string) as unknown as string;
+                value.value = value.value;
+            });
+            let margin = { top: 100, right: 20, bottom: 100, left: 70 },
+                width = 960 - margin.left - margin.right,
+                height = 700 - margin.top - margin.bottom;
+            let svg = d3
+                .select(svgRef.current)
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', `translate(${margin.left}, ${margin.top})`);
+            let x = d3
+                .scaleTime()
+                .domain(
+                    // @ts-ignore
+                    d3.extent(data, d => {
+                        return d.date;
+                    }),
+                )
+                .range([0, width]);
+            let y = d3
+                .scaleLinear()
+                .domain(
+                    // @ts-ignore
+                    d3.extent(data, d => Number(d.value)),
+                )
+                .range([height, 0]);
+            svg.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
+            svg.append('g').call(d3.axisLeft(y));
+            let valueLine = d3
+                .line()
+                .x(d => {
+                    // @ts-ignore
+                    return x(d.date);
+                })
+                .y(d => {
+                    // @ts-ignore
+                    return y(d.value);
+                });
+
+            // @ts-ignore
+            svg.append('path').data([data]).attr('class', 'line').attr('fill', 'none').attr('stroke', 'royalblue').attr('stroke-width', 1.5).attr('d', valueLine);
+        };
+        setData();
+    }, []);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <h2 style={{ padding: '5rem 0 0 2rem' }}>COVLID Line chart</h2>
+            <svg ref={svgRef}></svg>
+        </>
     );
 }
 
