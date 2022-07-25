eed70b102899266c1faae74e818ce78e0657518c
ParkSungHyun <psh1030.dev@gmail.com>
2022-07-25 11:35:10 +0900

    init: vite+typescript+react setting
    vite+typescript+react 개발환경 설정을 하였습니다.
    @emotion/react,d3를 설치하였습니다.

---
+++ linechart/pshdev1030/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { LineChart } from "./components/LineChart";
+
+function App() {
+  return (
+    <div>
+      <LineChart />
+    </div>
+  );
+}
+
+export default App;
---
+++ linechart/pshdev1030/src/components/LineChart.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useRef } from "react";
+import { css } from "@emotion/react";
+import { select } from "d3";
+
+export const LineChart = () => {
+  return <div></div>;
+};
