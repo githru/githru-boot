a6056b16220db6b52ccf6146c95e3708ec5d86d9
jaeylee <jaeylee@smilegate.com>
2022-07-26 20:08:58 +0900

    add: normalize css 추가

--- linechart/ag502/src/index.tsx
+++ linechart/ag502/src/index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './assets/css/normalize.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
