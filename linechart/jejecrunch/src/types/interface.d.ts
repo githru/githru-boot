c4bd7402264c8b86566b6752d32bba1320d5ac37
제제jeje <gkswlgp456@gmail.com>
2022-07-27 02:02:58 +0900

    feat(LineChart1): Add Line Chart

---
+++ linechart/jejecrunch/src/types/interface.d.ts
@@ -0,0 +1,20 @@
+export interface Data {
+    x: number;
+    y1: number;
+    y2: number;
+    y3: number;
+}
+
+export interface InputData {
+    date: string;
+    value: string;
+}
+
+export interface LineChartData {
+    d: string;
+    v: number;
+}
+
+export interface Iterable<T> {
+    [Symbol.iterator](): Iterable<T>;
+}
