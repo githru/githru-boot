ecc337c6c818a69b26e9007f05b49f317ec5e00c
jaeylee <jaeylee@smilegate.com>
2022-07-26 21:16:28 +0900

    add: data fetch 로직 추가

--- linechart/ag502/src/App.tsx
+++ linechart/ag502/src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import * as d3 from 'd3';
+
+import { AppleStockColumnType } from './types';
+
+// @ts-ignore
+import AppleStockCSV from './assets/data/appleStock.csv';
+
+const lengthInfo = {}
 
 function App() {
-    return <div className="App">line chart</div>;
+    const [appleStockData, setAppleStockData] = useState<d3.DSVRowArray<AppleStockColumnType> | never[]>([]);
+
+    const fetchAppleStockData = useCallback(async () => {
+        try {
+            const data: d3.DSVRowArray<AppleStockColumnType> = await d3.csv(AppleStockCSV);
+            setAppleStockData(data);
+        } catch (error) {
+            setAppleStockData([]);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchAppleStockData();
+    }, []);
+
+    return (
+        <div className="App">
+            <svg />
+        </div>
+    );
 }
 
 export default App;
