c186dd60b6512f84c9cc4f483acfe3bed4a55d0e
제제jeje <gkswlgp456@gmail.com>
2022-07-28 00:53:24 +0900

    feat: Add link line chart 1 and 2 in Main

--- linechart/jejecrunch/src/App.tsx
+++ linechart/jejecrunch/src/App.tsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LineChart2 from "./components/LineChart2";
 import LineChart from "./components/Linechart";
 import { lineData } from "./data/line";
+import { Main } from "./pages/Main";
 
 function App() {
     return (
-        <div className="App">
-            <div className="App-header">
-                <LineChart height={400} values={lineData} />
-            </div>
-        </div>
+        <RecoilRoot>
+            <Router>
+                <div className="App">
+                    <div className="App-header"></div>
+
+                    <Routes>
+                        <Route path="/" element={<Main />} />
+                        <Route
+                            path="/line-chart/1"
+                            element={<LineChart height={400} values={lineData} />}
+                        />
+                        <Route path="/line-chart/2" element={<LineChart2 height={400} />} />
+                    </Routes>
+                </div>
+            </Router>
+        </RecoilRoot>
     );
 }
 
