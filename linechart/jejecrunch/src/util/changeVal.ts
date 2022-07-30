f3c67777a9270baac73c36b4d4cffac7b419360e
제제jeje <gkswlgp456@gmail.com>
2022-07-31 02:49:14 +0900

    fix: Remove optional chaining

--- linechart/jejecrunch/src/util/changeVal.ts
+++ linechart/jejecrunch/src/util/changeVal.ts
@@ -1,21 +1,21 @@
-import { InputData, Type } from "../types/interface";
+import { InputData, Category } from '../types/interface';
 
 const init: [number, number][] = [];
 
-export default function changeVal(d: InputData[], { type }: Type) {
-    const result = d?.reduce((prev, cur: InputData) => {
-        if (type === "ts_file") {
-            prev.push([Number(cur.date), Number(cur.value)]);
-        } else if (type === "csv_url") {
-            let chd = 0;
-            if (cur.date) {
-                if (typeof cur.date !== "string") chd = cur.date.getTime();
-                else chd = Number(cur.date);
-            }
-            prev.push([chd, Number(cur.value)]);
-        }
-        return prev;
-    }, init);
+export default function changeVal(d: InputData[], { type }: Category) {
+	const result = d.reduce((prev, cur: InputData) => {
+		if (type === 'ts_file') {
+			prev.push([Number(cur.date), Number(cur.value)]);
+		} else if (type === 'csv_url') {
+			let chd = 0;
+			if (cur.date) {
+				if (typeof cur.date !== 'string') chd = cur.date.getTime();
+				else chd = Number(cur.date);
+			}
+			prev.push([chd, Number(cur.value)]);
+		}
+		return prev;
+	}, init);
 
-    return result.slice(0, d.length - 1);
+	return result.slice(0, d.length - 1);
 }
