ef841f1a299966feecb04570497ae665dc9a8f5a
제제jeje <gkswlgp456@gmail.com>
2022-07-30 15:18:03 +0900

    feat: Add Recoil init

--- linechart/jejecrunch/src/App.tsx
+++ linechart/jejecrunch/src/App.tsx
@@ -1,31 +1,20 @@
-import React from "react";
-import { RecoilRoot } from "recoil";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LineChart2 from "./components/LineChart2";
-import LineChart from "./components/Linechart";
-import { lineData } from "./data/line";
-import { Main } from "./pages/Main";
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import OneLineChart from './pages/OneLineChart/OneLineChart';
+import Main from './pages/Main';
 
 function App() {
-    return (
-        <RecoilRoot>
-            <Router>
-                <div className="App">
-                    <div className="App-header"></div>
-
-                    <Routes>
-                        <Route path="/" element={<Main />} />
-                        <Route
-                            path="/line-chart/1"
-                            element={<LineChart height={400} values={lineData} />}
-                        />
-                        <Route path="/line-chart/2" element={<LineChart2 height={400} />} />
-                    </Routes>
-                </div>
-            </Router>
-        </RecoilRoot>
-    );
+	return (
+		<div className="App">
+			<div className="App-header" />
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/line-chart/1" element={<OneLineChart number={1} />} />
+				<Route path="/line-chart/2" element={<OneLineChart number={2} />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
