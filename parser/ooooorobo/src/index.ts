955a01937a7039085480ff7a5a83e1048d37635d
조예진 <orobos654@gmail.com>
2022-07-30 17:49:02 +0900

    refactor: 의미 있는 이름으로 변경

--- parser/ooooorobo/src/index.ts
+++ parser/ooooorobo/src/index.ts
@@ -14,7 +14,7 @@ function runGitLogCommand() {
      * - @begin@ 부모 해시, 자기 해시, 작성자 이름, 작성자 메일, 커밋 날짜, 커밋 내용
      * - 추가된 라인, 삭제된 라인, 파일명 (n줄)
      */
-        // TODO 1: 커밋 정보에 들어갈 값을 옵션으로 분리
+    // TODO 1: 커밋 정보에 들어갈 값을 옵션으로 분리
     const stdout = execSync(
             `git log --reverse --pretty=${LOG_PREFIX}%p${LOG_DELIMITER}%h${LOG_DELIMITER}%an${LOG_DELIMITER}%ae${LOG_DELIMITER}%cd${LOG_DELIMITER}%s --numstat | tail -n ${MAX_LOG_COUNT}`,
             {cwd: REPO_PATH}
@@ -29,13 +29,13 @@ function runGitLogCommand() {
 function parseLogIntoCommit(log: string): Commit[] {
     return log
         .split(`${LOG_PREFIX}`)
-        .filter(x => Boolean(x))
+        .filter(commit => Boolean(commit))
         .map(commitLog => {
             const lines = commitLog.split('\n');
             // TODO: git log 실행할 때 pretty 옵션에 줬던 순서와 동일하도록 한 곳에서 관리하기
             const [parentHash, commitHash, authorName, authorMail, committedAt, title] = lines[0].split(LOG_DELIMITER);
             const editedFileInfoList: EditedFileInfo[] = lines.splice(1)
-                .filter(x => Boolean(x))
+                .filter(line => Boolean(line))
                 .map(line => {
                     const [addedLineCount, removedLineCount, fileName] = line.trim().split('\t')
                     return {
