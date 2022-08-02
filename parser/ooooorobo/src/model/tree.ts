f2b1f7a7168fa167b8c41d0ada142126f55119ce
조예진 <orobos654@gmail.com>
2022-08-02 21:17:12 +0900

    feat: Tree 구현

---
+++ parser/ooooorobo/src/model/tree.ts
@@ -0,0 +1,93 @@
+// 참고: https://www.30secondsofcode.org/articles/s/js-data-structures-tree
+class TreeNode<T> {
+    private _key: string;
+    private _value: T;
+    private _parent: TreeNode<T> | null;
+    private _children: TreeNode<T>[] = [];
+
+    constructor (key: string, value: T, parent: TreeNode<T> | null = null) {
+        this._key = key;
+        this._value = value;
+        this._parent = parent;
+    }
+
+    get key(): string {
+        return this._key;
+    }
+
+    get value(): T {
+        return this._value;
+    }
+
+    get parent(): TreeNode<T> | null {
+        return this._parent;
+    }
+
+    get children(): TreeNode<T>[] {
+        return this._children;
+    }
+
+    get isLeaf(): boolean {
+        return Boolean(this._children.length);
+    }
+}
+
+class Tree<T> {
+    private root: TreeNode<T>;
+
+    constructor(key: string, value: T) {
+        this.root = new TreeNode<T>(key, value, null);
+    }
+
+    /**
+     * 특정 node로부터 전위순회
+     * @param node
+     */
+    public* preOrderTraversal(node = this.root): Generator<TreeNode<T>> {
+        yield node;
+        if (!node.isLeaf) {
+            for (const child of node.children) {
+                yield* this.preOrderTraversal(child);
+            }
+        }
+    }
+
+    /**
+     * 특정 노드에 새로운 값을 삽입
+     * @param parentKey
+     * @param key
+     * @param child
+     */
+    public insert(parentKey: string, key: string, child: T): boolean {
+        const parentNode = this.find(parentKey);
+        if (!parentNode) {
+            return false;
+        }
+        parentNode.children.push(new TreeNode<T>(key, child));
+        return true;
+    }
+
+    /**
+     * key 값을 가지는 노드를 Tree에서 삭제
+     * @param key
+     */
+    public remove(key: string): void {
+        for (const node of this.preOrderTraversal(this.root)) {
+            if (node.key === key) {
+                node.parent?.children.filter(child => child.key !== key);
+            }
+        }
+    }
+
+    /**
+     * key와 일치하는 노드 찾기
+     * 일치하는 노드가 없으면 null을 반환
+     * @param key
+     */
+    public find(key: string): TreeNode<T> | null {
+        for (const node of this.preOrderTraversal(this.root)) {
+            if (node.key === key) return node;
+        }
+        return null;
+    }
+}
