8148c4848b8d8a52d683aac1d97ee497f3f0a4d4
ytaek.kim <ytaek.kim@hcil.snu.ac.kr>
2022-09-14 16:44:45 +0900

    feat: re-new github crawler

--- crawler/ytaek/stat.js
+++ crawler/ytaek/stat.js
@@ -1,59 +1,123 @@
 const axios = require('axios');
+const fs = require('fs');
 
-// axios.get(`https://api.github.com/repos/githru/githru-feature-practices/pulls?state=all&per_page=1000`)
-// .then(res => {
-
-// console.log(res.data);
-//     const prs = [];
-//     res.data.map(pr => {
-//         prs.push(pr.number);
-//     })
-// console.log(prs);
-    
-// }).catch(err => console.log(err));
-
-
-const prs = [
-    50, 49, 48, 47, 46, 45, 44, 43, 41, 38, 37,
-    36, 35, 34, 32, 31, 30, 29, 28, 27, 26, 25,
-    24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14,
-    13, 
-    12, 11, 10,  9,  8,  7,  
-    6,  5,  4,  3,
-     2
-  ];
+const fileName = "a.txt";
 
 const token = ``;
-
+const repo = "githru/githru-vscode-ext";
+// const repo = "${repo}";
 const headers = {
     headers: {
         Authorization: `token ${token}`
     },
 }
-for (const prId of prs) {
-    axios.get(`https://api.github.com/repos/githru/githru-feature-practices/pulls/${prId}/comments`, headers)
-    .then(res => {
-        const comments = res.data;
-        comments.map(comment => {
-            console.log(`${prId} review_comment ${comment.user.login}`);
-        })
-    }).catch(err => console.log(err));
+const headerColumns = ["user", "category", "date", "message", "url"];
+const printStrs = [];
+const printLog = (user, category, date, url, message) => {
+    console.log(url);
+    printStrs.push([user, category, date, url, message].join("\t"));
+};
+
+const getAllPRList = async () => {
+    const prs = [];
+    const res = await axios.get(`https://api.github.com/repos/${repo}/pulls?state=all&per_page=1000`, headers);
     
-    axios.get(`https://api.github.com/repos/githru/githru-feature-practices/issues/${prId}/comments`, headers)
-    .then(res => {
-        const comments = res.data;
-        comments.map(comment => {
-            console.log(`${prId} comment ${comment.user.login}`);
-        })
-    }).catch(err => console.log(err));
-    
-    axios.get(`https://api.github.com/repos/githru/githru-feature-practices/pulls/${prId}/reviews`, headers)
-    .then(res => {
-        const reviews = res.data;
-        reviews
-        .filter(review => review.body !== "")
-        .map(review => {
-            console.log(`${prId} review ${review.user.login}`);
-        })
-    }).catch(err => console.log(err));    
+    res.data.forEach(pr => {
+        prs.push(pr.number);
+    });    
+    console.log(prs);
+    return prs;
 }
+
+const getAllIssueList = async () => {
+    const issues = [];
+    const res = await axios.get(`https://api.github.com/repos/${repo}/issues?state=all&per_page=1000`, headers);
+// console.log(res.data);
+    res.data
+        .filter(data => data.pull_request === undefined)
+        .forEach(issue => {
+            issues.push(issue.number);
+    });    
+    console.log(issues);
+    return issues;
+}
+
+
+const printByAuthor = async (url, category, dateName = 'created_at', bodyName="body") => {
+    const res = await axios.get(url, headers);
+// console.log(res.data);
+    const dataList = [].concat(res.data);
+    dataList.forEach(data => {
+        printLog(data.user.login, category, data[dateName], data.html_url ?? url, data[bodyName].replace(/\r?\n|\r/g, ""));
+    });
+}
+
+const prs = [
+    180, 176, 173, 171, 163, 162, 161, 160, 159, 158, 157, 156,
+    155, 154, 153, 152, 151, 150, 143, 142, 141, 140, 139, 137,
+    136, 135, 134, 133, 132, 131, 130, 129, 127, 122, 121, 120,
+    118, 115, 114, 112, 111, 109, 108, 107, 106, 105, 103, 101,
+    100,  98,  97,  96,  95,  94,  93,  92,  91,  89,  88,  87,
+     86,  83,  82,  81,  80,  76,  75,  74,  73,  72,  67,  66,
+     63,  62,  61,  59,  54,  53,  52,  51,  50,  49,  48,  47,
+     46,  45,  44,  43,  42,  41,  39,  38,  36,  35,  34,  33,
+     30,  26,  20,  19
+  ];
+const issues = [
+    179, 178, 177, 175, 174, 172, 170,
+    169, 168, 167, 166, 165, 164, 149,
+    148, 147, 146, 145, 144, 138, 128,
+    126, 125, 124, 123, 119, 117, 116,
+    113, 110, 104, 102,  90,  85,  84
+  ];
+
+console.log(headerColumns.join("\t"));
+  
+const main = async () => {
+    //const prs = await getAllPRList();
+    // for (const prId of prs) {
+    //     let url = `https://api.github.com/repos/${repo}/pulls/${prId}`;
+    //     await printByAuthor(url, "pull_request", "created_at", "title");
+    // };
+
+    // for (const prId of prs) {
+    //     let url = `https://api.github.com/repos/${repo}/pulls/${prId}/comments`;
+    //     await printByAuthor(url, "pull_request_review_inline_comment");
+    // };
+
+    // for (const prId of prs) {
+    //     let url = `https://api.github.com/repos/${repo}/issues/${prId}/comments`;
+    //     await printByAuthor(url, "pull_request_comment");
+    // };
+
+    // for (const prId of prs) {
+    //     let url = `https://api.github.com/repos/${repo}/pulls/${prId}/reviews`;
+    //     await printByAuthor(url, "pull_request_review", "submitted_at");
+    // };
+
+    // //////////////////////////////////////
+    // // ISSUE 
+    // const issues = await getAllIssueList();    
+
+    // for (const issueId of issues) {
+    //     let url = `https://api.github.com/repos/${repo}/issues/${issueId}`;
+    //     await printByAuthor(url, "issue", "created_at", "title");
+    // };
+
+    // for (const issueId of issues) {
+    //     let url = `https://api.github.com/repos/${repo}/issues/${issueId}/comments`;
+    //     await printByAuthor(url, "issue_comment");
+    // };
+
+    ////////////////////////////////////
+    // reactions
+    const issueAndPrs = prs.concat(issues);
+    for (const id of issueAndPrs) {
+        let url = `https://api.github.com/repos/${repo}/issues/${id}/reactions`;
+        await printByAuthor(url, "reactions", "created_at", "content");
+    }
+
+    fs.appendFile(fileName, printStrs.join("\n") + "\n", 'utf8', function(err) {});
+};
+
+main();
