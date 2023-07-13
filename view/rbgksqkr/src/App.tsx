65f8f6007dff4119abcff0ccf3326139c5f311cf
rbgksqkr <rbgks1937@naver.com>
2023-07-13 16:08:22 +0900

    feat: string 데이터를 위치 데이터로 변환 및 범위 설정

--- view/rbgksqkr/src/App.tsx
+++ view/rbgksqkr/src/App.tsx
@@ -6,27 +6,36 @@ function App() {
     const ref = useRef<HTMLDivElement>(null);
     const width = 300;
     const height = 300;
-    const [data, setData] = useState(lineData);
-
-    const parseDate = (date: string) => {
-        const formatTime = d3.timeParse("%Y-%m-%d");
-        return formatTime(date);
-    };
+    const [values, setValues] = useState(lineData);
 
     const removeSvg = () => {
         d3.select(ref.current).call((g) => g.select("circle").remove());
     };
 
     useEffect(() => {
+        const margin = { top: 20, right: 30, bottom: 30, left: 40 };
         const currentElement = ref.current;
         const svg = d3.select(currentElement);
-        svg.append("svg")
-            .append("circle")
-            .attr("r", 5)
-            .attr("cx", 30)
-            .attr("cy", 50)
-            .attr("fill", "red");
-    }, [data]);
+
+        const parseDate: any = d3.timeParse("%Y-%m-%d");
+        const data = values.map(({ d, v }) => ({
+            d: parseDate(d),
+            v,
+        }));
+
+        const xDomain = d3.extent(data, (data) => data.d) as [number, number];
+        const x = d3
+            .scaleUtc()
+            .domain(xDomain)
+            .range([margin.left, width - margin.right]);
+
+        const yMax = d3.max(data, (data) => data.v) as number;
+        const y = d3
+            .scaleLinear()
+            .domain([0, yMax])
+            .nice()
+            .range([height - margin.bottom, margin.top]);
+    }, [values]);
 
     return (
         <>
