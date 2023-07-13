468903e0c7c516abe1c30d11085f994b7f10b0a0
rbgksqkr <rbgks1937@naver.com>
2023-07-13 14:06:07 +0900

    feat: d3 라이브러리로 그려보기

--- view/rbgksqkr/src/App.tsx
+++ view/rbgksqkr/src/App.tsx
@@ -1,5 +1,33 @@
+import * as d3 from "d3";
+import { useEffect, useRef, useState } from "react";
+
 function App() {
-    return <div className="App">초기세팅</div>;
+    const ref = useRef<SVGSVGElement>(null);
+    const width = 300;
+    const height = 300;
+    const [data, setData] = useState([40, 80, 150, 160, 230, 260]);
+
+    useEffect(() => {
+        const currentElement = ref.current;
+        const svg = d3.select(currentElement);
+        svg.append("circle")
+            .attr("r", 5)
+            .attr("cx", width / 5)
+            .attr("cy", height / 2)
+            .attr("fill", "red");
+    }, [data]);
+
+    return (
+        <>
+            <h1> Line Chart </h1>
+            <svg
+                className="container"
+                ref={ref}
+                width={width}
+                height={height}
+            ></svg>
+        </>
+    );
 }
 
 export default App;
