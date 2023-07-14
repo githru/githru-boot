b538e7d88812ef6875b18504228f1c9f8001d67e
yura <rladbfk0000@naver.com>
2023-07-14 22:57:19 +0900

    chore : 프로젝트 재빌드 && data 업로드

---
+++ view/yura0302/postcss.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  parser: 'postcss',
+  plugins: [],
+};
--- view/yura0302/src/component/LineChart.tsx
+++ view/yura0302/src/component/LineChart.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App = () => {
+const LineChart = () => {
   return <div></div>;
 };
 
-export default App;
+export default LineChart;
