d9ea06c0f9ddbe6d9e4a17eddf1fd2bb5f8d43c5
kimdonggu42 <kdonggoo44@gmail.com>
2023-07-14 20:17:17 +0900

    chore: 시각화(날씨) 데이터 추가

--- view/kimdonggu42/src/App.tsx
+++ view/kimdonggu42/src/App.tsx
@@ -1,5 +1,11 @@
+import LineChart from './components/LineChart';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <LineChart />
+    </div>
+  );
 }
 
 export default App;
---
+++ view/kimdonggu42/src/components/LineChart.tsx
@@ -0,0 +1,5 @@
+function LineChart() {
+  return <div></div>;
+}
+
+export default LineChart;
