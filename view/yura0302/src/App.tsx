377171db542f8b9fc35bcc950cfcef4005c6b866
yura <rladbfk0000@naver.com>
2023-07-15 16:37:17 +0900

    docs : 리드미 수정

--- view/yura0302/src/App.tsx
+++ view/yura0302/src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+// @ts-nocheck
+import React, { useRef, useState } from 'react';
+import * as d3 from 'd3';
 
 const App = () => {
-  return <div></div>;
+  const svgRef = useRef();
+
+  const width = 640;
+  const height = 400;
+  const margin = 20;
+
+  const x = d3
+    .scaleUtc()
+    .domain(d3.extent(data, (d) => d.date))
+    .range([0, width])
+    .nice();
+
+  const xAxis = d3.axisBottom(x).tickFormat((d) => d3.timeFormat('%b %d')(d));
+  svg.append('g').attr('transform', `translate(0, ${height})`).call(xAxis);
+
+  const svg=d3.select
+
+  return (
+    <div>
+      <div> 왜 안될까</div>
+    </div>
+  );
 };
 
 export default App;
--- view/yura0302/src/component/LineChart.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LineChart = () => {
-  return <div></div>;
-};
-
-export default LineChart;
