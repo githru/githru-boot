a0cf11947e6a5968733baa53375c322a84ea7955
kimdonggu42 <kdonggoo44@gmail.com>
2023-07-15 17:39:50 +0900

    feat: x, y축 추가

--- view/kimdonggu42/src/components/LineChart.tsx
+++ view/kimdonggu42/src/components/LineChart.tsx
@@ -1,5 +1,70 @@
+import { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+import { weatherData } from '../data/weatherData';
+
+interface DateInterface {
+  date: Date;
+  temp: number;
+}
+
 function LineChart() {
-  return <div></div>;
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const width = 1000;
+    const height = 400;
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+    const currentElement = divRef.current;
+
+    const data = weatherData.map((value) => ({
+      date: new Date(value.date),
+      temp: value.temp,
+    }));
+
+    const x = d3.scaleUtc(d3.extent(data, (d) => d.date) as [Date, Date], [
+      margin.left,
+      width - margin.right,
+    ]);
+
+    const y = d3
+      .scaleLinear([d3.min(data, (d) => d.temp), d3.max(data, (d) => d.temp)] as [number, number], [
+        height - margin.bottom,
+        margin.top,
+      ])
+      .nice();
+
+    const line = d3
+      .line<DateInterface>()
+      .x((d) => x(d.date))
+      .y((d) => y(d.temp));
+
+    const svg = d3
+      .select(currentElement)
+      .call((g) => g.select('svg').remove())
+      .append('svg')
+      .attr('viewBox', `0,0,${width},${height}`);
+
+    const xAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) =>
+      g.attr('transform', `translate(0,${height - margin.bottom})`).call(
+        d3
+          .axisBottom(x)
+          .ticks(width / 80)
+          .tickSizeOuter(0),
+      );
+
+    const yAxis = (g: d3.Selection<SVGGElement, unknown, null, undefined>) =>
+      g.attr('transform', `translate(${margin.left},0)`).call(d3.axisLeft(y));
+
+    svg.append<SVGGElement>('g').call(xAxis);
+    svg.append<SVGGElement>('g').call(yAxis);
+  }, []);
+
+  return (
+    <>
+      <h1> Line Chart </h1>
+      <div ref={divRef} />
+    </>
+  );
 }
 
 export default LineChart;
