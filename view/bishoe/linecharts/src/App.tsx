16d034b6e4597a3b90d5a196b39200feb4665c85
bishoe01 <bishoe01@gmail.com>
2023-07-15 14:50:13 +0900

    add : example코드로 d3 테스트해보기

--- view/bishoe/linecharts/src/App.tsx
+++ view/bishoe/linecharts/src/App.tsx
@@ -1,10 +1,28 @@
-import "./App.css";
-import Test from "./components/Test";
+import * as d3 from "d3";
+import { useEffect, useRef, useState } from "react";
+
 function App() {
+  const ref = useRef<SVGSVGElement>(null);
+  const width = 300;
+  const height = 300;
+  const [data, setData] = useState([40, 80, 150, 160, 230, 260]);
+
+  useEffect(() => {
+    const currentElement = ref.current;
+    const svg = d3.select(currentElement);
+    svg
+      .append("circle")
+      .attr("r", 25)
+      .attr("cx", width / 5)
+      .attr("cy", height / 2)
+      .attr("fill", "green");
+  }, [data]);
+
   return (
-    <section>
-      <Test />
-    </section>
+    <>
+      <h1> Line Chart </h1>
+      <svg className="container" ref={ref} width={width} height={height}></svg>
+    </>
   );
 }
 
