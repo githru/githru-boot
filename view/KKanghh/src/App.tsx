5fd1a33345bbbb197744269905f26a90c09c3869
kkanghh <attsky2@gmail.com>
2023-07-16 13:16:58 +0900

    feat: 축 그리기

--- view/KKanghh/src/App.tsx
+++ view/KKanghh/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useEffect, useState } from "react";
+import "./App";
+import * as d3 from "d3";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([24, 30, 45, 70, 26]);
+  const svgRef = useRef(null);
+  const width = 640;
+  const height = 400;
+  const marginTop = 20;
+  const marginRight = 20;
+  const marginBottom = 30;
+  const marginLeft = 40;
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current); // selection 객체
+    const x = d3
+      .scaleUtc()
+      .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+      .range([marginLeft, width - marginRight]);
+    const y = d3
+      .scaleLinear()
+      .domain([0, 100])
+      .range([height - marginBottom, marginTop]);
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - marginBottom})`)
+      .call(d3.axisBottom(x));
+
+    svg
+      .append("g")
+      .attr("transform", `translate(${marginLeft}, 0)`)
+      .call(d3.axisLeft(y));
+  }, [data]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <svg width={width} height={height} ref={svgRef}></svg>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
