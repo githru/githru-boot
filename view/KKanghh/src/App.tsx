621ddd2f96b0db4824e5d0c1b3b1ef7a4c996b7a
kkanghh <attsky2@gmail.com>
2023-07-16 22:02:29 +0900

    feat: LineChart - 2022년 제주 평균 기온

--- view/KKanghh/src/App.tsx
+++ view/KKanghh/src/App.tsx
@@ -1,41 +1,55 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, useMemo } from "react";
 import "./App";
 import * as d3 from "d3";
+import WeatherLine from "./Graph/WeatherLine";
 
 function App() {
-  const [data, setData] = useState([24, 30, 45, 70, 26]);
+  const [showData, setShowData] = useState({ showJeju: false });
+  const [jejuData, setJejuData] = useState<{ date: Date; average: number }[]>(
+    []
+  );
   const svgRef = useRef(null);
-  const width = 640;
-  const height = 400;
-  const marginTop = 20;
-  const marginRight = 20;
-  const marginBottom = 30;
-  const marginLeft = 40;
+  const width = useMemo(() => 640, []);
+  const height = useMemo(() => 400, []);
+  const marginTop = useMemo(() => 20, []);
+  const marginRight = useMemo(() => 20, []);
+  const marginBottom = useMemo(() => 30, []);
+  const marginLeft = useMemo(() => 40, []);
+  const [x, setX] = useState<d3.ScaleTime<number, number, never> | null>(null);
+  const [y, setY] = useState<d3.ScaleLinear<number, number, never> | null>(
+    null
+  );
 
   useEffect(() => {
-    const svg = d3.select(svgRef.current); // selection 객체
-    const x = d3
-      .scaleUtc()
-      .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-      .range([marginLeft, width - marginRight]);
-    const y = d3
-      .scaleLinear()
-      .domain([0, 100])
-      .range([height - marginBottom, marginTop]);
-    svg
-      .append("g")
-      .attr("transform", `translate(0, ${height - marginBottom})`)
-      .call(d3.axisBottom(x));
-
-    svg
-      .append("g")
-      .attr("transform", `translate(${marginLeft}, 0)`)
-      .call(d3.axisLeft(y));
-  }, [data]);
+    const getData = async () => {
+      const csv = await d3.csv("/weather.csv", (row) => {
+        if (row.date && row.average)
+          return { date: new Date(row.date), average: +row.average };
+        else return { date: new Date(), average: 0 };
+      });
+      setJejuData(csv);
+    };
+    getData();
+  }, []);
 
   return (
     <>
-      <svg width={width} height={height} ref={svgRef}></svg>
+      <button
+        onClick={() => setShowData((state) => ({ ...state, showJeju: true }))}
+      >
+        제주
+      </button>
+      {jejuData && (
+        <WeatherLine
+          data={jejuData}
+          width={width}
+          height={height}
+          marginTop={marginTop}
+          marginRight={marginRight}
+          marginBottom={marginBottom}
+          marginLeft={marginLeft}
+        />
+      )}
     </>
   );
 }
