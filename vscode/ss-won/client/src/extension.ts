7bbe87a267969ab7511c5bbb7c10a8cb37127fea
Sowon Jung <sowonjung@smartmind.team>
2023-07-19 00:35:51 +0900

    feat: implement lsp example(prefix-emoji-sentence-lsp)

--- vscode/ss-won/client/src/extension.ts
+++ vscode/ss-won/client/src/extension.ts
@@ -1,25 +1,47 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "lsp-example" is now active!');
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('lsp-example.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from client!');
-  });
+let client: LanguageClient;
 
-  context.subscriptions.push(disposable);
+export function activate(context: ExtensionContext) {
+  // The server is implemented in node
+  const serverModule = context.asAbsolutePath(path.join('server', 'dist', 'index.js'));
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+
+  // Start the client. This will also launch the server
+  client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
--- vscode/ss-won/server/index.ts
+++ vscode/ss-won/server/index.ts
@@ -1,1 +1,152 @@
-// server/index.ts
+import {
+  createConnection,
+  ProposedFeatures,
+  TextDocuments,
+  TextDocumentSyncKind,
+  TextDocumentPositionParams,
+  Diagnostic,
+  DiagnosticSeverity,
+  InitializeParams,
+  InitializeResult,
+  CompletionItemKind,
+  CompletionItem,
+} from 'vscode-languageserver/node';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+const connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+const documents = new TextDocuments(TextDocument);
+
+let hasDiagnosticRelatedInformationCapability: boolean = false;
+
+connection.onInitialize((params: InitializeParams) => {
+  const capabilities = params.capabilities;
+
+  hasDiagnosticRelatedInformationCapability = !!(
+    capabilities.textDocument &&
+    capabilities.textDocument.publishDiagnostics &&
+    capabilities.textDocument.publishDiagnostics.relatedInformation
+  );
+
+  const result: InitializeResult = {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      // Tell the client that this server supports code completion.
+      completionProvider: {
+        resolveProvider: true,
+      },
+    },
+  };
+
+  return result;
+});
+// The content of a text document has changed. This event is exmitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent(change => {
+  validateTextDocument(change.document);
+});
+
+const validateTextDocument = async (textDocument: TextDocument) => {
+  const diagnostics: Diagnostic[] = [];
+  textDocument
+    .getText()
+    .split('\n')
+    .forEach((text, line) => {
+      const trimmedText = text.trim();
+      if (trimmedText.length === 0) {
+        diagnostics.push({
+          severity: DiagnosticSeverity.Warning,
+          range: {
+            start: { line, character: 0 },
+            end: { line, character: text.length },
+          },
+          message: 'sentence is empty.',
+        });
+        return;
+      }
+      if (!checkIsStartWithEmoji(text)) {
+        const diagnostic: Diagnostic = {
+          severity: DiagnosticSeverity.Error,
+          range: {
+            start: { line, character: 0 },
+            end: { line, character: text.length },
+          },
+          message: `sentence is not started with emoji!!!😵`,
+        };
+        if (hasDiagnosticRelatedInformationCapability)
+          diagnostic.relatedInformation = [
+            {
+              location: {
+                uri: textDocument.uri,
+                range: { ...diagnostic.range },
+              },
+              message: 'sentence syntax error',
+            },
+          ];
+        diagnostics.push(diagnostic);
+      }
+    });
+
+  // Send the computed diagnostics to VSCode.
+  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+};
+
+const checkIsStartWithEmoji = (text: string) => {
+  const emojiRegex = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+  return emojiRegex.test(text.trim());
+};
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion((textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+  const textDocument = documents.get(textDocumentPosition.textDocument.uri)?.getText();
+  const { character } = textDocumentPosition.position;
+
+  return (textDocument && !checkIsStartWithEmoji(textDocument)) || character === 0
+    ? [
+        {
+          label: '📌 Notice',
+          kind: CompletionItemKind.Text,
+          data: 1,
+        },
+        {
+          label: '👣 History',
+          kind: CompletionItemKind.Text,
+          data: 2,
+        },
+        {
+          label: '🔥 Hot',
+          kind: CompletionItemKind.Text,
+          data: 3,
+        },
+      ]
+    : [];
+});
+
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+  switch (item.data) {
+    case 1:
+      item.detail = 'Notice';
+      item.documentation = 'notification or warning of something.';
+      break;
+    case 2:
+      item.detail = 'History';
+      item.documentation = 'the whole series of past events connected with someone or something.';
+      break;
+    case 3:
+      item.detail = 'Hot';
+      item.documentation = 'new and exciting things.';
+  }
+  return item;
+});
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
