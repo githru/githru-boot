fd86903a127ca502862850ca3f02387671c59d24
root <root@DESKTOP-J22D89D>
2023-07-24 01:41:35 +0900

    add: example data

---
+++ view/taesung/chart/src/data/example.ts
@@ -0,0 +1,32 @@
+import { ColumnType } from '../types';
+
+export const example: ColumnType[] = [
+  {
+    day: 'Monday',
+    value: 1,
+  },
+  {
+    day: 'Tuesday',
+    value: 2,
+  },
+  {
+    day: 'Wednesday',
+    value: 3,
+  },
+  {
+    day: 'Thursday',
+    value: 4,
+  },
+  {
+    day: 'Friday',
+    value: 5,
+  },
+  {
+    day: 'Saturday',
+    value: 6,
+  },
+  {
+    day: 'Sunday',
+    value: 7,
+  },
+]
