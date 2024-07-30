502d1cd06a9145941478c68b415b52b74a440c52
pithesun <wonjh0730@naver.com>
2024-07-31 00:59:25 +0900

    해수욕장 선택 컴포넌트 UI 오류 수정

--- view/pithesun-test/src/app/Component/SearchBeach.tsx
+++ view/pithesun-test/src/app/Component/SearchBeach.tsx
@@ -1,35 +1,28 @@
 "use client";
 
-import Image from "next/image";
-import { RedirectType, redirect } from "next/navigation";
-import { getData } from "@/api/api";
 import { beaches } from "@/constants/beachname";
-import { useEffect, useState } from "react";
-import IBeach from "@/interface/beach";
 import { useRouter } from "next/navigation";
 
 export default function SearchBeach({ beachNum }) {
   const router = useRouter();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <label>
-        해수욕장을 선택하세요.
-        <select
-          value={beachNum}
-          onChange={(e) => {
-            e.preventDefault();
-            const num = e.target.value;
-            console.log("num", num);
-            router.push(`/?num=${num}`);
-          }}
-        >
-          {beaches.map((beach) => (
-            <option key={beach.num} value={beach.num}>
-              {beach.name}
-            </option>
-          ))}
-        </select>
-      </label>
-    </main>
+    <label>
+      해수욕장을 선택하세요.
+      <select
+        value={beachNum}
+        onChange={(e) => {
+          e.preventDefault();
+          const num = e.target.value;
+
+          router.push(`/?num=${num}`);
+        }}
+      >
+        {beaches.map((beach) => (
+          <option key={beach.num} value={beach.num}>
+            {beach.name}
+          </option>
+        ))}
+      </select>
+    </label>
   );
 }
