0dfe217d7cd5733109e2d4f6ea14f9ffc768ed8e
shgusgh12 <https://github.com/shgusgh12>
2024-07-31 04:43:52 +0900

    feat: 차트 제목 작성

--- view/shgusgh12/src/App.tsx
+++ view/shgusgh12/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>2022년 성북구 강수량</h1>
+      <LineChart />
+      <BarChart />
+    </div>
+  );
 }
 
 export default App;
